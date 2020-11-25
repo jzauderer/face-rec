444da4ae95d4e90273ba85a37a0ed14315bca247
Justin Zauderer <jzauds@gmail.com>
2020-11-25 14:24:12 -0500

    Tweaked html and art assets

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -34,7 +34,7 @@ video.addEventListener('play', ()=>{
         new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
         const resizedDetections = faceapi.resizeResults(detections, displaySize)
         canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-        faceapi.draw.drawDetections(canvas, resizedDetections)
         faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
     }, 100)
 })
